c449576187519638cff114d4bbbfb8810819aaf6
jefry <jefrycen@thamrin.co.id>
2021-06-03 12:01:45 +0700

    ready to release

--- lib/api/contents.dart
+++ lib/api/contents.dart
@@ -5,6 +5,7 @@ class News{
     var res = await utils.post(
         {
           "corp": globVar.auth.corp,
+          "cust_id": globVar.user.CUST_ID,
           "type": news,
         },
         globVar.hostRest + "/contents/", secure: true, many: true);
