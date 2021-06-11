fafbef085e1ccbdc43dd1204093977ff00853d8e
jefry <jefrycen@thamrin.co.id>
2021-06-11 21:49:48 +0700

    step

--- lib/CustomShape/splash_screen.dart
+++ lib/CustomShape/splash_screen.dart
@@ -55,58 +55,6 @@ class _SplashscreenState extends State<Splashscreen> {
             fit: BoxFit.fill
           )
         ),
-        // child: Stack(
-        //   children: [
-        //     Container(
-        //       height: MediaQuery.of(context).size.height,
-        //       width: MediaQuery.of(context).size.width,
-        //       child: CustomPaint(
-        //         painter: RoundPainter(),
-        //       ),
-        //     ),
-        //     Column(
-        //       children: [
-        //         Container(
-        //           height: MediaQuery.of(context).size.height*0.44,
-        //           padding: EdgeInsets.only(top:MediaQuery.of(context).size.height*0.16,left: MediaQuery.of(context).size.width*0.24,right: MediaQuery.of(context).size.width*0.24,bottom: MediaQuery.of(context).size.height*0.05),
-        //           child: Container(
-        //             height: 218,
-        //             width: 205,
-        //             decoration: BoxDecoration(
-        //                 image: DecorationImage(
-        //                   image: AssetImage("images/icon.png"),
-        //                   // colorFilter: ColorFilter.mode(Color.fromRGBO(10, 10, 249, 0.5), BlendMode.modulate ),
-        //                   fit: BoxFit.fitHeight,
-        //                 )
-        //             ),
-        //           ),
-        //         ),
-        //         Container(
-        //           child: Text("Customer Loyalty",style: TextStyle(color: Colors.black.withOpacity(0.75),fontSize: 28,fontWeight: FontWeight.w700, fontFamily: "Roboto",fontStyle: FontStyle.normal),),),
-        //         Container(
-        //           child: Text("You don't earn loyalty in a day",style: TextStyle(color: Colors.black.withOpacity(0.75),fontSize: 16,fontWeight: FontWeight.w300, fontFamily: "Roboto",fontStyle: FontStyle.normal),),),
-        //         Expanded(
-        //           child: Center(
-        //             child: Column(
-        //               mainAxisSize: MainAxisSize.min,
-        //               children: [
-        //                 Text(state,style: TextStyle(fontSize: 19,fontWeight: FontWeight.w500),),
-        //                 SizedBox(height: 10,),
-        //                 Container(
-        //                   width: MediaQuery.of(context).size.width*0.5,
-        //                     child: LinearProgressIndicator(
-        //                       value: percentage,
-        //                     )),
-        //                 SizedBox(height: 10,),
-        //                 Text("${(percentage*100).toStringAsFixed(0)}%",style: TextStyle(fontSize: 21,),)
-        //               ],
-        //             ),
-        //           ),
-        //         )
-        //       ],
-        //     ),
-        //   ],
-        // ),
       ),
     );
   }
